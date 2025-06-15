f07ae415d1d772f45c6ca148442e9d08c1348600
andi lauw <andi.safariansyah7@gmail.com>
2025-06-15 15:18:13 +0700

    first-comit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   safelist: ["bg-red-500", "text-white", "p-4"],
   theme: {
     extend: {},
